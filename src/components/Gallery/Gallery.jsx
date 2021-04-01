7fb6493258531b666c2257b0d4e321267507e65b
Maria Akifieva <mitafrumoasa@gmail.com>
2021-04-01 16:18:13 +0300

    added Gallery settings, fixed more styles

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,13 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import Popup from '../Popup/Popup'
 import { mapStateToProps } from '../../reducers/index'
 import { addToFavoritesAC, removeFromFavoritesAC } from '../../actions/index'
 import './Gallery.scss'
 
-const Gallery = ({favorites, movies, dispatch}) => {
+const Gallery = ({genres, favorites, movies, dispatch}) => {
   const [isList, setIsList] = useState(false);
   const [shownMovie, showPopup] = useState(null);
+  const [displayedMovies, setDisplayedMovies] = useState([]);
+  const [genre, setGenre] = useState();
+
+  useEffect(() => {
+    const displayedMovies = movies && movies.map(movie => {
+      if(!genre || movie.genres.includes(genre)) {
+      return <div
+        key={movie.id}
+        className={`gallery__movie movie ${isList ? 'list' : ''}`}
+        onClick={() => showPopup(movie)}>
+
+        <button
+        className={`gallery__movie-label ${movie.isFavorite ? 'favorite' : ''}`}
+        onClick={(event) => addToFavorites(event, movie)} />
+
+        <div className="gallery__movie__image-container">
+          <img className="gallery__movie__image-container-image" src={movie.img} alt="" />
+        </div>
+
+        <div className="gallery__movie-info">
+          <p className="gallery__movie-info-text">
+            {movie.name}
+            {!isList && <br/ >}
+            {movie.year}
+          </p>
+
+          {isList && <>
+            <p className="gallery__movie-info-text">{movie.description}</p>
+            <ul className="gallery__movie-info__genres">
+            {movie.genres.map(genre =>
+              <li key={genre} className="gallery__movie-info__genres-genre">{genre}</li>
+            )}
+            </ul>
+          </>}
+        </div>
+
+      </div>
+    } else {
+      return null
+    }
+    })
+    setDisplayedMovies(displayedMovies)
+  }, [genre, movies])
 
   const addToFavorites = (event, movie) => {
     event.stopPropagation()
@@ -22,21 +65,26 @@ const Gallery = ({favorites, movies, dispatch}) => {
     <h3 className="gallery-title">Movies Gallery</h3>
 
     <div className="gallery__settings">
-      {/* <div className="genre-select">Select Genre</div>
-      <div className="view-as">view as: <button className="card-view" onClick={() => setIsList(false)} /> <button className="list-view" onClick={() => setIsList(true)} /></div> */}
+      <select
+        name="genres"
+        onChange={event => setGenre(event.target.value)}
+        className="gallery__settings__genre-select"
+      >
+        <option value="" className="gallery__settings__genre-select-option">Select Genre</option>
+        {genres.map(genre =>
+          <option value={genre} key={genre} className="gallery__settings__genre-select-option">
+            {genre}
+          </option>
+        )}
+    </select>
+      <div className="gallery__settings__view-as">View As:
+        <button className="gallery__settings__view-as-card" onClick={() => setIsList(false)} />
+        <button className="gallery__settings__view-as-list" onClick={() => setIsList(true)} />
+      </div>
     </div>
 
     {
-      movies && movies.map(movie =>
-        (<div key={movie.id} className={`gallery__movie movie ${isList ? 'list' : ''}`} onClick={() => showPopup(movie)}>
-          <button className={`gallery__movie-label ${movie.isFavorite ? 'favorite' : ''}`} onClick={(event) => addToFavorites(event, movie)} />
-          <div className="gallery__movie__image-container">
-            <img className="gallery__movie__image-container-image" src={movie.img} alt="" />
-          </div>
-          <p className="gallery__movie-info">{movie.name} <br/ >
-          {movie.year}</p>
-        </div>)
-      )
+      displayedMovies
     }
 
     <Popup movie={shownMovie} showPopup={showPopup}/>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,19 @@
 const initialState = {
+  genres: [],
   movies: [],
   favorites: [],
 }
 
 export const mapStateToProps = (state) => {
   return state;
+}
+
+const getAllGenres = (movies) => {
+  let genres = []
+  movies.forEach((movie, i) => {
+    genres = [...genres, ...movie.genres]
+  });
+  return [...new Set(genres)] // removes dublicates from array (source: https://medium.com/dailyjs/how-to-remove-array-duplicates-in-es6-5daa8789641c)
 }
 
 export const rootReducer = (state = initialState, action) => {
@@ -15,7 +24,8 @@ export const rootReducer = (state = initialState, action) => {
         let movieIndex = newMovies.findIndex(movie => movie.id == favorite.id)
         if (movieIndex > -1) newMovies[movieIndex]['isFavorite'] = true
       })
-      return {...state, movies: newMovies}
+      const genres = getAllGenres(action.movies)
+      return {...state, movies: newMovies, genres}
     }
     case 'FAVORITES_RECEIVED': {
       return {...state, favorites: action.favorites}
@@ -38,7 +48,7 @@ export const rootReducer = (state = initialState, action) => {
       const movieIndex = state.movies.findIndex(movie => movie.id === action.id) //here id = arrIndex + 1, but in case it's not findIndex() is used
       const newMovies = [...state.movies]
       newMovies[movieIndex]['isFavorite'] = false
-      
+
       return {...state, movies: newMovies, favorites: newFavorites}
     }
     default:
